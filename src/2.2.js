5e18b1df3b5b069f197c0e787a20a1d16de0a44b
Charles Berube <cberube@gmail.com>
2022-12-05 09:17:21 -0800

    Day 2 Part 2 refactor

--- src/2.2.js
+++ src/2.2.js
@@ -7,34 +7,20 @@ const dataPath = path.join(__dirname, process.argv[2])
 const data = inputTools.loadInputFile(dataPath)
 
 /**
- * There isn't really any *more* data in this case, it is
- * just differently organized. This object maps the shape
- * played by our opponent to an object which lists the
- * scores for each outcome. X for losing, Y for a tie,
- * and Z for winning.
- * 
- * We could add another layer of lookups to determine the
- * scores for the outcomes and shapes, but they are fixed
- * and there are still only 9 of them, so we can just list
- * them here. Once again, the format is (shape score) +
- * (win/loss score) for clarity
+ * Looking at this again for a moment, there really isn't even
+ * a reason to bother with the nested structure -- the only thing
+ * that needs to change is the score for each pair.
  */
-const responseMap = {
-  A: {
-    X: 3 + 0,
-    Y: 1 + 3,
-    Z: 2 + 6
-  },
-  B: {
-    X: 1 + 0,
-    Y: 2 + 3,
-    Z: 3 + 6
-  },
-  C: {
-    X: 2 + 0,
-    Y: 3 + 3,
-    Z: 1 + 6
-  }
+ const outcomeMap = {
+  'A X': 3 + 0,
+  'A Y': 1 + 3,
+  'A Z': 2 + 6,
+  'B X': 1 + 0,
+  'B Y': 2 + 3,
+  'B Z': 3 + 6,
+  'C X': 2 + 0,
+  'C Y': 3 + 3,
+  'C Z': 1 + 6,
 }
 
 function application() {
@@ -42,13 +28,9 @@ function application() {
   // copy-pasta or accidental formatting
   const lines = data.split('\n').filter(line => line.trim().length > 0)
 
-  // Translate each line into a score. Conveniently when we split
-  // the line on spaces we get an array of [shape, result] which
-  // is precisely the format we need for `R.pathOr`.
   const score = R.compose(
     R.sum,
-    R.map(pair => R.pathOr(0, pair, responseMap)),
-    R.map(line => line.split(' '))
+    R.map(line => outcomeMap[line.trim()] || 0)
   )(lines)
 
   console.log(score)
